90739212f0a7d20c96bdad70c6385f8d754dda0d
git <git@example.com>
2019-10-23 13:56:26 +0000

    adding the blue green deployment line

--- new.php
+++ new.php
@@ -2,4 +2,5 @@
 <h1> new file in the docker. The file is updated. </h1>
 <h1> this is the second updarte for k8s </h1>
 <h1> pussing the image without the tag </h1>
+<h1> this is for blue-green deployment </h1>
 </html>
