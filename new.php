e549828e2ed46383b5516a2ed7decbd77f42936a
vikashoo7 <34591866+vikashoo7@users.noreply.github.com>
2019-10-26 13:19:18 +0530

    Update new.php

--- new.php
+++ new.php
@@ -4,4 +4,5 @@
 <h1> pussing the image without the tag </h1>
 <h1> this is for blue-green deployment </h1>
 <h1> This message shows that successful deployment </h1>
+ <h1> Editing from homw </h1>
 </html>
